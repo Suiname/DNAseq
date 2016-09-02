1823eb7419759b0ad40de77c9b334da793e46a8b
Jason Tham <jason.tham@tempus.com>
2016-09-01 20:22:24 -0500

    linting

--- naive_matching.py
+++ naive_matching.py
@@ -1,4 +1,8 @@
+"""Collection of string matching utilities for DNA sequences."""
+
+
 def naive(p, t):
+    """Return the naive match of a substring to a string."""
     occurrences = []
     for i in range(len(t) - len(p) + 1):  # loop over alignments
         match = True
@@ -10,14 +14,18 @@ def naive(p, t):
             occurrences.append(i)  # all chars matched; record
     return occurrences
 
+
 def reverseComplement(s):
+    """Return the reverse complement of a string."""
     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
     t = ''
     for base in s:
         t = complement[base] + t
     return t
 
+
 def readGenome(filename):
+    """Return a sequence from a fasta file."""
     genome = ''
     with open(filename, 'r') as f:
         for line in f:
@@ -26,7 +34,9 @@ def readGenome(filename):
                 genome += line.rstrip()
     return genome
 
+
 def readFastq(filename):
+    """Return a tuple of sequences and qualities from a fastq file."""
     sequences = []
     qualities = []
     with open(filename) as fh:
@@ -34,23 +44,27 @@ def readFastq(filename):
             fh.readline()  # skip name line
             seq = fh.readline().rstrip()  # read base sequence
             fh.readline()  # skip placeholder line
-            qual = fh.readline().rstrip() # base quality line
+            qual = fh.readline().rstrip()  # base quality line
             if len(seq) == 0:
                 break
             sequences.append(seq)
             qualities.append(qual)
     return sequences, qualities
 
+
 # implement a version of naive matching that is strand aware
 def sa_naive(p, t):
+    """Return a naive string match that considers the reverse omplement."""
     occurrences = []
-    if reverseComplement(p) == p:
-        return naive(p, t)
-    occurrences = naive(p, t)
-    occurrences += naive(reverseComplement(p), t)
+    if reverseComplement(p) == p:  # if the reverse complement is the same,
+        return naive(p, t)  # just return the naive match
+    occurrences = naive(p, t)  # otherwise, set result to be the naive match
+    occurrences += naive(reverseComplement(p), t)  # then add in the complement
     return occurrences
 
+
 def naive_2mm(p, t):
+    """Return a naive string match allowing up to 2 mismatches."""
     occurrences = []
     for i in range(len(t) - len(p) + 1):  # loop over alignments
         match = True
