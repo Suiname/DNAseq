d2ad0bbda7e6257a8451747be7015352f1e62735
Jason Tham <jtham@sbcglobal.net>
2016-09-05 23:13:37 -0500

    initial skeleton of week 2

---
+++ Week 2/better_matching.py
@@ -0,0 +1,21 @@
+def boyer_moore(p, p_bm, t):
+    """ Do Boyer-Moore matching. p=pattern, t=text,
+        p_bm=BoyerMoore object for p """
+    i = 0
+    occurrences = []
+    while i < len(t) - len(p) + 1:
+        shift = 1
+        mismatched = False
+        for j in range(len(p)-1, -1, -1):
+            if p[j] != t[i+j]:
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
+                mismatched = True
+                break
+        if not mismatched:
+            occurrences.append(i)
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
+        i += shift
+    return occurrences
