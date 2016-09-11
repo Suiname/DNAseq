18b65380f4d2a764e8073ba768a7da413978d896
Jason Tham <jason.tham@tempus.com>
2016-09-11 15:51:16 -0500

    implemented Boyer Moore with counts

--- Week 2/better_matching.py
+++ Week 2/better_matching.py
@@ -1,6 +1,8 @@
+"""Collection of string matching utilities for DNA sequences."""
+
+
 def boyer_moore(p, p_bm, t):
-    """ Do Boyer-Moore matching. p=pattern, t=text,
-        p_bm=BoyerMoore object for p """
+    """Do Boyer-Moore matching."""
     i = 0
     occurrences = []
     while i < len(t) - len(p) + 1:
@@ -19,3 +21,51 @@ def boyer_moore(p, p_bm, t):
             shift = max(shift, skip_gs)
         i += shift
     return occurrences
+
+
+def naive_with_counts(p, t):
+    """Return the naive match.
+
+    Also return the number of alignments
+    and the total number of character comparisons.
+    """
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):  # loop over alignments
+        match = True
+        for j in range(len(p)):  # loop over characters
+            if t[i+j] != p[j]:  # compare characters
+                match = False
+                break
+        if match:
+            occurrences.append(i)  # all chars matched; record
+    return occurrences
+
+
+def bm_with_counts(p, p_bm, t):
+    """Do Boyer-Moore matching.
+
+    Return the number of matches, the number of alignments
+    and the total number of character comparisons.
+    """
+    i = 0
+    occurrences = []
+    alignments = 0
+    comparisons = 0
+    while i < len(t) - len(p) + 1:
+        shift = 1
+        alignments += 1
+        mismatched = False
+        for j in range(len(p)-1, -1, -1):
+            comparisons += 1
+            if p[j] != t[i+j]:
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
+                mismatched = True
+                break
+        if not mismatched:
+            occurrences.append(i)
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
+        i += shift
+    return occurrences, alignments, comparisons
