7ba6e4056354fdf08acf8b7654974ec6ed6ca174
Jason Tham <jason.tham@tempus.com>
2016-09-19 13:59:26 -0500

    got em all wrong :-(

--- Week 4/assembly.py
+++ Week 4/assembly.py
@@ -39,3 +39,72 @@ def scs(ss):
         if shortest_sup is None or len(sup) < len(shortest_sup):
             shortest_sup = sup  # found shorter superstring
     return shortest_sup  # return shortest
+
+
+def all_scs(ss):
+    """Return all shortest common superstrings."""
+    shortest_sup = None
+    for ssperm in itertools.permutations(ss):
+        sup = ssperm[0]  # superstring starts as first string
+        for i in range(len(ss)-1):
+            # overlap adjacent strings A and B in the permutation
+            olen = overlap(ssperm[i], ssperm[i+1], min_length=1)
+            # add non-overlapping portion of B to superstring
+            sup += ssperm[i+1][olen:]
+        if shortest_sup is None or len(sup) <= len(shortest_sup):
+            if shortest_sup is not None:
+                shortest_sup.append(sup)
+            else:
+                shortest_sup = [sup]
+    return shortest_sup  # return shortest
+
+
+def pick_maximal_overlap(reads, k):
+    """Return maximal overlap of length k in a given set of reads."""
+    reada, readb = None, None
+    best_olen = 0
+    for a, b in itertools.permutations(reads, 2):
+        olen = overlap(a, b, min_length=k)
+        if olen > best_olen:
+            reada, readb = a, b
+            best_olen = olen
+    return reada, readb, best_olen
+
+
+def greedy_scs(reads, k):
+    """Return shortest common superstring using greedy algorithm."""
+    read_a, read_b, olen = pick_maximal_overlap(reads, k)
+    while olen > 0:
+        reads.remove(read_a)
+        reads.remove(read_b)
+        reads.append(read_a + read_b[olen:])
+        read_a, read_b, olen = pick_maximal_overlap(reads, k)
+    return ''.join(reads)
+
+
+def readGenome(filename):
+    """Return a sequence from a fasta file."""
+    genome = ''
+    with open(filename, 'r') as f:
+        for line in f:
+            # ignore header line with genome information
+            if not line[0] == '>':
+                genome += line.rstrip()
+    return genome
+
+
+def readFastq(filename):
+    """Return a tuple of sequences and qualities from a fastq file."""
+    sequences = []
+    qualities = []
+    with open(filename) as fh:
+        while True:
+            fh.readline()  # skip name line
+            seq = fh.readline().rstrip()  # read base sequence
+            fh.readline()  # skip placeholder line
+            qual = fh.readline().rstrip()  # base quality line
+            if len(seq) == 0:
+                break
+            sequences.append(seq)
+            qualities.append(qual)
+    return sequences, qualities
---
+++ Week 4/test.py
@@ -0,0 +1,12 @@
+"""Run code to answer quiz questions for week 4."""
+
+from assembly import scs, all_scs, readFastq, greedy_scs
+strings = ['CCT', 'CTT', 'TGC', 'TGG', 'GAT', 'ATT']
+print("SCS result: ", scs(strings))
+print("SCS all result: ", all_scs(strings))
+reads, qualities = readFastq('ads1_week4_reads.fq')
+genome_length = 15894
+full_genome = greedy_scs(reads, genome_length)
+print("Fully assembled genome", full_genome)
+print("Number of As: ", full_genome.count("A"))
+print("Number of Ts: ", full_genome.count("T"))
